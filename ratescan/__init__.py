4cd84fe815f3f29a37056b3aafe62e9b992b5036
Jens Buss <jens.buss@online.de>
2018-09-21 16:43:53 +0200

    move to subpackage conversion

--- ratescan/__init__.py
+++ ratescan/__init__.py
@@ -1,28 +1,4 @@
-import gzip
-import pandas as pd
-import logging
+from pkg_resources import resource_string
 
-def jsonToDf(infile, keys):
-    '''
-    Covert a json file to a data frame
-    '''
-    log = logging.getLogger(__name__)
-    
-    open_func = open
+__version__ = resource_string('fact', 'VERSION').decode().strip()
 
-    log.info("reading: {}".format(infile))
-    if infile.endswith(".gz"):
-        open_func = gzip.open
-
-    dfs = []
-    lines = 0
-    with open_func(infile) as f:
-        for line in f:
-            df = pd.read_json(line)
-            df = df[default_keys_to_store]
-            dfs.append(df)
-            lines+=1
-
-    res = pd.concat(dfs)
-    logger.info("extracted {} thresholds from {} lines".format(len(res), lines))
-    return res
